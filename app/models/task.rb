bf8e2649d90cbebff5efde8c6ddd74862b00f649
qa3aq <dimdac@gmail.com>
2021-11-23 14:28:17 +0500

    addstate_machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,30 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :in_development do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+
+    event :in_qa do
+      transition in_development: :in_qa
+    end
+
+    event :in_code_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :ready_for_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :released do
+      transition ready_for_release: :released
+    end
+
+    event :archived do
+      transition [:new_task, :released] => :archived
+    end
+  end
 end
