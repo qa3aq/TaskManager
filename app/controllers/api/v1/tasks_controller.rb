154c6ed80260c0d39cbbbdb6bb61952a7bb97e20
qa3aq <dimdac@gmail.com>
2021-12-01 20:38:39 +0500

    added destroy

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -22,16 +22,23 @@ class Api::V1::TasksController < Api::V1::ApplicationController
     respond_with(task, serializer: TaskSerializer, location: nil)
   end
 
-  private
-
-  def task_params
-    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
-  end
-
   def update
     task = Task.find(params[:id])
     task.update(task_params)
 
     respond_with(task, serializer: TaskSerializer)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    respond_with(task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
   end
 end
