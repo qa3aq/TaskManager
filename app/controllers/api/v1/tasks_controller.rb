9050dcc9af880118625fcf8825806c2c0b085fe2
qa3aq <dimdac@gmail.com>
2021-12-19 15:04:24 +0500

    added desc sort

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::TasksController < Api::V1::ApplicationController
       ransack(ransack_params).
       result.
       page(page).
-      per(per_page)
+      per(per_page).
+      order(id: :desc)
 
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
