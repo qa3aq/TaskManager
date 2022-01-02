58041efad4179ca07f97b5c3fbab6fff4154fb92
qa3aq <dimdac@gmail.com>
2022-01-02 21:23:54 +0500

    moved states to presenter and added theme provider

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import store from 'store';
 import { Provider } from 'react-redux';
 import TaskBoard from 'containers/TaskBoard';
-import MUITheme from 'MUITheme/MUITheme';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme();
 
 const App = () => (
   <Provider store={store}>
-    <MUITheme>
+    <ThemeProvider theme={theme}>
       <TaskBoard />
-    </MUITheme>
+    </ThemeProvider>
   </Provider>
 );
 
