fd98299d168599f75f632eadd2a70547d44e62b0
qa3aq <dimdac@gmail.com>
2022-01-12 11:00:06 +0500

    added simplecov-lcov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-require "simplecov"
+require 'simplecov'
 
 SimpleCov.start do
-  if ENV["CI"]
-    require "simplecov-lcov"
+  if ENV['COVERAGE']
+    require 'simplecov-lcov'
 
     SimpleCov::Formatter::LcovFormatter.config do |c|
       c.report_with_single_file = true
-      c.single_report_path = "coverage/lcov.info"
+      c.lcov_file_name = 'lcov.info'
+      c.single_report_path = 'coverage/lcov.info'
     end
 
     formatter SimpleCov::Formatter::LcovFormatter
